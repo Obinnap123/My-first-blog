2edd5665e3788e98e3cbe2efba8cebf79dacfcea
Paul Obinna <paulobinna493@gmail.com>
2024-07-18 18:31:56 +0100

    ...

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,24 +4,27 @@ import styles from './styles.module.scss'
 
 export default function Header(){
   return <>
-    <header className={styles.header}>
-    <nav className={styles.navbar}>
-    
-    <div className= {styles['nav-container']}>
-    <ul className={styles['nav-links']}>
-    <li><a href="#home">Home</a></li>
-    <li><a href="#packagaes">Packages</a></li>
-    <li><a href="#testimonials">Testimonials</a></li>
-    <li><a href="#other">Other</a></li>
-  </ul>
-  </div>
-  <div className={styles['auth-links']}>
-        <a href="#signup" className={`${styles.authBtn} ${styles.signUpBtn}`}>Sign up</a>
-        <a href="#login" className={styles['auth-btn']}>Login</a>
-  </div>
-
-  </nav>
-</header>
+     <header className={`w-100 d-flex ${styles.header} position-fixed`}>
+      <nav className={`${styles.navbar} `}>
+        <a className="navbar-brand" href="#">
+          <img src='/logo.png' alt='logo' style={{ width: 90, height: 90 }} />
+        </a>
+        <ul className={`d-flex ${styles['nav-links']} list-unstyled `}>
+          <li><a href="#home">Home</a>
+          </li>
+          <li><a href="#packages">Packages</a>
+          </li>
+          <li><a href="#testimonials">Testimonials</a>
+          </li>
+          <li><a href="#other">Other</a>
+          </li>
+        </ul>
+        <div className={`${styles['auth-links']}`}>
+          <a className={` ${styles['authBtn']} ${styles.signUpBtn}`} href="#signup">Sign up</a>
+          <a className={` ${styles['auth-btn']} ${styles.loginBtn}`} href="#login">Login</a>
+        </div>
+      </nav>
+    </header>
   </>
 };
 
